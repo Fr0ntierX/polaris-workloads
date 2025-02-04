c4e7ce7da2234d4baa6df39b1c1f387e99b03f73
Karol Legut <karol.legut121@gmail.com>
2025-02-04 13:37:56 +0100

    feat: add Dockerfile and start script for Llama 3.3 model setup with GPU support

---
+++ polaris-llm/gpu/llama3.3/src/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check NVidia GPU
+nvidia-smi
+
+# Run Ollama
+export OLLAMA_HOST="0.0.0.0"
+ollama serve &
+ollama list
+ollama run $POLARIS_LLM_OLLAMA_MODEL --verbose --keepalive 24h
